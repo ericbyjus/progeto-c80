f9320470dfa96491b86d70f044f5b2c6c556a0f7
ericbyjus <113055083+ericbyjus@users.noreply.github.com>
2022-12-29 14:36:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+menuListArray = ["Pizza Vegetariana",//adicionar mais itens
+                "Pizza de frango",
+                "Pizza Portuguesa",
+                "Pizza Quatro Queijos",
+                "Pizza de Calabreza",
+                "Pizza Extravaganza",
+            ];
+
+function addItem(){
+var htmldata;
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=""
+for(var i=0;i<menuListArray.length;i++){
+    htmldata=htmldata+imgtags+menuListArray[i]+'<br>';
+}
+document.getElementById(displayAddedMenu).innerHTML = htmldata;
+}
+
+function getMenu(){ var htmldata=""; menuListArray.sort(); for(var i=0;i<menuListArray.length;i++){ htmldata=htmldata+ menuListArray[i] + '<br>' } document.getElementById("displayMenu").innerHTML = htmldata; } function addTop(){ var item=document.getElementById("addItem").value; menuListArray.push(item); addItem(); }
